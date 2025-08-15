357ffe54e8109e47d94e9efd30425fdab7c2ded3
Lucas Lima <lucaslimaneves1@hotmail.com>
2025-08-15 16:19:59 -0300

    Added POSCOMP to UTILS_BENCHMARKS.py
    Added the poscomp benchmark and refactored the OAB benchmark to use our data.

--- src/UTILS_BENCHMARKS.py
+++ src/UTILS_BENCHMARKS.py
@@ -220,11 +220,11 @@ class tweetsentbr:
 
 class oab:
     def __init__(self):
-        self.dataset_path = "eduagarcia/oab_exams"
+        self.dataset_path = "cemig-ceia/vestibulares_concatened"
         self.subset = None
-        self.split = "train"
-        self.important_columns = ["question", "choices", "answerKey"]
-        self.label_column = "answerKey"
+        self.split = "oab"
+        self.important_columns = ["statement", "alternatives", "answer"]
+        self.label_column = "answer"
         self.answer_pattern = "multiple_choice"
         self.answer_type = "category"
 
@@ -233,16 +233,49 @@ class oab:
         prompt_informations = {}
         prompt_informations['base_system_message'] = "Você é um assistente prestativo, responda de forma direta e objetiva."
 
-        num_alternatives_example = len(example['choices']['text'])
-        example_alternatives = [f"({chr(65 + asc_letter)}): {example['choices']['text'][asc_letter]}" for asc_letter in range(num_alternatives_example)]
+        alternatives_dict = ast.literal_eval(example['alternatives'])
+
+        num_alternatives_example = len(alternatives_dict['text'])
+        example_alternatives = [f"({chr(65 + asc_letter)}): {alternatives_dict['text'][asc_letter]}" for asc_letter in range(num_alternatives_example)]
         example_alternatives = "\n".join(example_alternatives)
         
-        prompt_informations['user_message'] = f"Pergunta:\n{example['question']}.\n\nLeia as alternativas abaixo e responda corretamente a pergunta:\n\n{example_alternatives}"
+        prompt_informations['user_message'] = f"Pergunta:\n{example['statement']}.\n\nLeia as alternativas abaixo e responda corretamente a pergunta:\n\n{example_alternatives}"
 
         prompt_informations['assistant_message_with_answer'] = f"Lendo as alternativas {''.join(('(' + chr(65 + asc_letter) + '),' for asc_letter in range(num_alternatives_example)))} a alternativa que responde corretamente a pergunta é a letra ({example['label']})"
         
         prompt_informations['assistant_message_without_answer'] = f"Lendo as alternativas {''.join(('(' + chr(65 + asc_letter) + '),' for asc_letter in range(num_alternatives_example)))} a alternativa que responde corretamente a pergunta é a letra ("
         return prompt_informations
+
+class poscomp:
+    def __init__(self):
+        self.dataset_path = "cemig-ceia/vestibulares_concatened"
+        self.subset = None
+        self.split = "poscomp"
+        self.important_columns = ["statement", "alternatives", "answer"]
+        self.label_column = "answer"
+        self.answer_pattern = "multiple_choice"
+        self.answer_type = "category"
+
+
+    def get_prompt_informations(self, example):
+        prompt_informations = {}
+        prompt_informations['base_system_message'] = "Você é um assistente prestativo, responda de forma direta e objetiva."
+        
+        alternatives_dict = ast.literal_eval(example['alternatives'])
+
+        num_alternatives_example = len(alternatives_dict['text'])
+        example_alternatives = [f"({chr(65 + asc_letter)}): {alternatives_dict['text'][asc_letter]}" for asc_letter in range(num_alternatives_example)]
+        example_alternatives = "\n".join(example_alternatives)
+        
+        
+        prompt_informations['user_message'] = f"Pergunta:\n{example['statement']}.\n\nLeia as alternativas abaixo e responda corretamente a pergunta:\n\n{example_alternatives}"
+
+        prompt_informations['assistant_message_with_answer'] = f"Lendo as alternativas {''.join(('(' + chr(65 + asc_letter) + '),' for asc_letter in range(num_alternatives_example)))} a alternativa que responde corretamente a pergunta é a letra ({example['label']})"
+        
+        prompt_informations['assistant_message_without_answer'] = f"Lendo as alternativas {''.join(('(' + chr(65 + asc_letter) + '),' for asc_letter in range(num_alternatives_example)))} a alternativa que responde corretamente a pergunta é a letra ("
+        return prompt_informations
+
+
 
 
 BENCHMARKS_INFORMATIONS = {
@@ -256,4 +289,6 @@ BENCHMARKS_INFORMATIONS = {
     "faquad": faquad(),
     "tweetsentbr": tweetsentbr(),
     "oab": oab(),
+    "poscomp": poscomp(),
     }
+
