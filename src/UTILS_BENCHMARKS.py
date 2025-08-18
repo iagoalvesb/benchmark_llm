bdc754a44d0734a11ae96376ab6edcfa71d0c7ac
Lucas Lima <lucaslimaneves1@hotmail.com>
2025-08-18 18:48:16 -0300

    Added the energy-regulacao benchmark to UTILS_BENCHMARKS.py

--- src/UTILS_BENCHMARKS.py
+++ src/UTILS_BENCHMARKS.py
@@ -275,6 +275,34 @@ class poscomp:
         prompt_informations['assistant_message_without_answer'] = f"Lendo as alternativas {''.join(('(' + chr(65 + asc_letter) + '),' for asc_letter in range(num_alternatives_example)))} a alternativa que responde corretamente a pergunta é a letra ("
         return prompt_informations
 
+class energy_regulacao:
+    def __init__(self):
+        self.dataset_path = "cemig-ceia/energy-eval"
+        self.subset = None
+        self.split = "train"
+        self.important_columns = ["right_context", "question", "choices", "answerKey"]
+        self.label_column = "answerKey"
+        self.answer_pattern = "multiple_choice"
+        self.answer_type = "category"
+
+
+    def get_prompt_informations(self, example):
+        prompt_informations = {}
+        prompt_informations['base_system_message'] = "Você é um assistente prestativo, responda de forma direta e objetiva."
+        
+        alternatives_dict = ast.literal_eval(example['choices'])
+
+        num_alternatives_example = len(alternatives_dict['text'])
+        example_alternatives = [f"({chr(65 + asc_letter)}): {alternatives_dict['text'][asc_letter]}" for asc_letter in range(num_alternatives_example)]
+        example_alternatives = "\n".join(example_alternatives)
+        
+        
+        prompt_informations['user_message'] = f"Tendo como base o contexto legal:\n{example['right_context']}\nSegue a pergunta:\n{example['question']}.\n\nLeia as alternativas abaixo e, tendo como base o contexto legal fornecido, responda corretamente a pergunta:\n\n{example_alternatives}"
+
+        prompt_informations['assistant_message_with_answer'] = f"Lendo as alternativas {''.join(('(' + chr(65 + asc_letter) + '),' for asc_letter in range(num_alternatives_example)))} a alternativa que responde corretamente a pergunta, com base no contexto legal fornecido, é a letra ({example['label']})"
+        
+        prompt_informations['assistant_message_without_answer'] = f"Lendo as alternativas {''.join(('(' + chr(65 + asc_letter) + '),' for asc_letter in range(num_alternatives_example)))} a alternativa que responde corretamente a pergunta, com base no contexto legal fornecido, é a letra ("
+        return prompt_informations
 
 
 
@@ -290,5 +318,5 @@ BENCHMARKS_INFORMATIONS = {
     "tweetsentbr": tweetsentbr(),
     "oab": oab(),
     "poscomp": poscomp(),
+    "energy_regulacao": energy_regulacao(),
     }
-
